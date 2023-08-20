ed42f645ab01f8b170a5aa053227b8f1ebdc34da
Deokcoding <72731197+Deokcoding@users.noreply.github.com>
2023-08-21 02:38:02 +0900

    momentum clone app
    노마드 코더님의 바닐라 자바스크립트 강의를 보고 만든 졸업 작품입니다.
    momentum 앱을 클론하였고, html, css, javascript를 이용해 만들었습니다.

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+
+const API_KEY = "13273247afb66bd799b8221adf3491b6";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
+}
+
+function onGeoError() {
+    alert("Can't find you. No wewather for you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
